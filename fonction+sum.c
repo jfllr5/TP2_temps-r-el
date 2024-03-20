5da1125628ba94b13ffb13eedf9a57b93c97607e
jfllr5 <juliane.follereau@gmail.com>
2024-03-20 09:32:59 +0100

    code avec la fonction qui fait la somme et le main dans le meme fichier

---
+++ fonction+sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int sum(int a, int b){
+return a+b;
+}
+
+int main(){
+int x = 87786, y = 876543;
+int result = sum(x, y);
+printf("la somme vaut : %d\n", result);
+return 0;
+}
